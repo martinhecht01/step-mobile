bddc7bf9b92e999b073ac41d008d9b7d4711788b
Manudithur <Manudithur@gmail.com>
2022-11-04 18:29:04 -0300

    feat: fondo

--- app/src/main/java/com/example/step_mobile/MainActivity.kt
+++ app/src/main/java/com/example/step_mobile/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.step_mobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.step_mobile.ui.theme.StepmobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +29,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    backGroundImg()
+                    Greeting("Pedro")
                 }
             }
         }
@@ -30,8 +39,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun backGroundImg(){
+    val image = painterResource(R.drawable.fondo);
+    Box(Modifier.fillMaxSize()) {
+        Image(painter = image, contentDescription = null)
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", Modifier.firstBaselineToTop(100.dp))
+}
+
+//Padding TOP
+fun Modifier.firstBaselineToTop(firstBaselineToTop: Dp) = layout { measurable, constraints ->
+    // Measure the composable
+    val placeable = measurable.measure(constraints)
+
+    // Check the composable has a first baseline
+    check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+    val firstBaseline = placeable[FirstBaseline]
+
+    // Height of the composable with padding - first baseline
+    val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+    val height = placeable.height + placeableY
+    layout(placeable.width, height) {
+        // Where the composable gets placed
+        placeable.placeRelative(0, placeableY)
+    }
 }
 
 @Preview(showBackground = true)
