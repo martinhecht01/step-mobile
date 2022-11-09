1df1673434df0cfc726a7e63731c1ac0ae438882
Manudithur <Manudithur@gmail.com>
2022-11-09 11:52:37 -0300

    feat: colors

--- app/src/main/java/com/example/step_mobile/ui/theme/Color.kt
+++ app/src/main/java/com/example/step_mobile/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package com.example.step_mobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val darkBlueBackground = Color(0x1B56ED)
+val lightBlueBackground = Color(0xBAE2FF)
+val blueBackground = Color(0x55B8FF)
+val greenText = Color(0x33D657)
+val greyText = Color(0x5A6175)
