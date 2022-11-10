c7bd1080183aa8bbc53f17ea45fd6079d1927e1b
TobiasPerry <tperry@itba.edu.ar>
2022-11-10 12:52:38 -0300

    exercisecard and playscreen

---
+++ app/src/main/java/com/example/step_mobile/components/ExerciseCard.kt
@@ -0,0 +1,32 @@
+package com.example.step_mobile.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconToggleButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ExerciseCard(title: String, description: String, Reps: Int) {
+    val paddingModifier = Modifier.padding(10.dp)
+    Card(shape = RoundedCornerShape(20.dp),elevation = 10.dp, modifier = paddingModifier) {
+        Column(verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = title, modifier = paddingModifier)
+            Text(text = description, modifier = paddingModifier)
+            Text( text = Reps.toString(), modifier = paddingModifier)
+        }
+    }
+}
